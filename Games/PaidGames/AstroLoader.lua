1f8596878827ad0e531483679464eb4cd35be79b
MineCraftVN098 <linhxida102@gmail.com>
2022-03-23 21:07:41 +0700

    Update AstroLoader.lua

--- Games/PaidGames/AstroLoader.lua
+++ Games/PaidGames/AstroLoader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-    loadstring(game:HttpGet'https://raw.githubusercontent.com/AstroScripter/ASTRO-HUB/main/Games/PaidGames/Scripts/BloxFruits.lua')()
+    getfenv()['loadstring'](game:HttpGet('https://raw.githubusercontent.com/AstroScripter/ASTRO-HUB/main/Games/PaidGames/Scripts/BloxFruits.lua'))()
 else
     game.Players.LocalPlayer:Kick('\n[ Astro Hub - Pre ]\nGame Not Support!')
 end
