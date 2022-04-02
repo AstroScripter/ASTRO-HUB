253ccdf6884a0a25735610d6cf12e664f719c4da
MineCraftVN098 <linhxida102@gmail.com>
2022-04-02 13:36:04 +0700

    Create AstroLoader.lua

---
+++ Games/FreeGames/AstroLoader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 8396586868 then
+    getfenv()['loadstring'](game:HttpGet('https://raw.githubusercontent.com/AstroScripter/ASTRO-HUB/main/Games/FreeGames/Scripts/AOG.lua'))()
+else
+    game.Players.LocalPlayer:Kick('\n[ Astro Hub ]\nGame Not Support!')
+end
