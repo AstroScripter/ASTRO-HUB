f52674c3b90996371018a762bde245d224ee75fb
MineCraftVN098 <linhxida102@gmail.com>
2022-04-20 21:45:01 +0700

    Update AstroLoader.lua

--- Games/FreeGames/AstroLoader.lua
+++ Games/FreeGames/AstroLoader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == nil then
     getfenv()['loadstring'](game:HttpGet('https://raw.githubusercontent.com/AstroScripter/ASTRO-HUB/main/Games/FreeGames/Scripts/.lua'))()
+elseif game.PlaceId == 205224386 then
+    getfenv()['loadstring'](game:HttpGet('https://raw.githubusercontent.com/AstroScripter/ASTRO-HUB/main/Games/FreeGames/Scripts/H&S.lua'))()
 else
     game.Players.LocalPlayer:Kick('\n[ Astro Hub ]\nGame Not Support!')
 end
